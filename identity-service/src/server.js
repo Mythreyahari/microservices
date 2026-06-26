7d800b1a1750189e14c5303b82ca55778084d02e
Mythreyahari <mythreyan53@gmail.com>
2026-06-26 19:48:59 +0530

    server.js implementing

--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -5,8 +5,15 @@ const { connectToDb } = require("./utils/mongodb");
 const helmet = require("helmet");
 const cors = require("cors");
 const app = express();
+const { RateLimiterRedis } = require("rate-limiter-flexible");
+const Redis = require("ioredis");
+const { rateLimit } = require("express-rate-limit");
+const {RedisStore} = require("rate-limit-redis")
 // connect the database
 connectToDb();
+
+// Redis
+const redisClient = new Redis(process.env.REDIS_URL);
 
 // middleware
 app.use(helmet());
@@ -17,3 +24,45 @@ app.use((req, res, next) => {
   logger.info(`Request body, ${req.body}`);
   next();
 });
+
+// Ddos protection and rate limitting
+const rateLimitter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 10,
+  duration: 1,
+});
+
+app.use((req, res, next) => {
+  rateLimitter
+    .consume(req.ip)
+    .then(() => next())
+    .catch((e) => {
+      logger.warn(`Rate limitter exceeded to the ip:${req.ip}`);
+      return res.status(429).json({
+        succees: false,
+        message: "Too many requests",
+      });
+    });
+});
+
+const sensitiveEndpointsLimitter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`sensitive rate limit exceeded for ip:${req.ip}`);
+    return res.status(429).json({
+      succees: false,
+      message: "Too many requests",
+    });
+  },
+  store:new RedisStore({
+    sendCommand:(...args)=>redisClient.call(...args)
+  })
+});
+ 
+
+
+
